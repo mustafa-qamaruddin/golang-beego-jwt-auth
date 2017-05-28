e6c21125a174c34a3c20b747d7b50dd94991dd62
Mustafa Qamar-ud-Din <m.qamaruddin@mqubits.com>
2017-05-28 03:06:07 +0200

    prod heroku http port

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	_ "passapp-engine-api/routers"
 	"github.com/astaxie/beego"
+	"strconv"
+    "os"
 )
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	} else {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+        if err == nil {
+                beego.HttpPort = port
+        }
 	}
 	beego.Run()
 }
